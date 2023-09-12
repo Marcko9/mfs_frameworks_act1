fd18be6143e6627021c3b3d8b18b6e88d69046f8
Marcko <marco.ramireze@gmail.com>
2023-09-11 21:18:01 -0600

    Update Adding props

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,20 @@
-import imagen from '../assets/img/premier-league-intro.png';
+import imagenLocal from '../assets/img/premier-league-intro.png';
 
-export function Post() {
+export function Post({likes, autor, imagen, comments, date}) {
   return (
     <div className="container mt-2">
         <div className="card" >
-            <img src={ imagen } className="card-img-top" alt="Imagen"/>
+            <img src={ imagenLocal } className="card-img-top" alt="Imagen" />
+            <img src={ imagen } className="card-img-top" alt="Imagen" style={{minHeight:"15em", maxHeight:"15em"}}/>
             <div className="card-body">
                 <div className="d-flex justify-content-end">
-                    <h5 className="postDate me-auto">3 min ago</h5>
-                    <button className='btn btn-danger'><i className="fa-solid fa-heart"/>43k</button>
+                    <h5 className="postDate me-auto">{ date }</h5>
+                    <button className='btn btn-danger'><i className="fa-solid fa-heart"/>{likes || 1}k</button>
                 </div>
-                <h5 className="userInfo">@eric</h5>
+                <h5 className="userInfo">{autor}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 
-                <h6 className="card-subtitle comment"><i className="fa-regular fa-message me-2 comment"/>Comments (15)</h6>
+                <h6 className="card-subtitle comment"><i className="fa-regular fa-message me-2 comment"/>Comments ({ comments })</h6>
             </div>
         </div>
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,8 @@
 export function SearchBar(){
     return (
-        <div className="container mt-2">
+        <div className="container mt-2" style={{maxWidth:"600px"}}>
             <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
             </form>
         </div>
     );
